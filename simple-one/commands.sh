a40587c1aa06ef596e25d634f9cff31513141663
joheiss <josef.heiss@jovisco.de>
2024-09-09 12:53:22 +0200

    mvn wrapper

--- simple-one/commands.sh
+++ simple-one/commands.sh
@@ -1,1 +1,5 @@
 mvn archetype:generate -DgroupId=com.jovisco -DartifactId=simple-one -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+
+# generate Maven wrapper - with version
+mvn -N io.takari:maven:wrapper -Dmaven=3.9.8
+
