849eb246a1c230b17c7bffad064e402e85d95c51
Josef Heiss <josef.heiss@jovisco.de>
2024-09-12 17:42:59 +0200

    1st for test w/junit-4 and 5

--- simple-one/src/test/java/com/jovisco/App4Test.java
+++ simple-one/src/test/java/com/jovisco/App4Test.java
@@ -1,27 +1,27 @@
 package com.jovisco;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * JUnit 4 tests
  */
-public class AppTest {
+public class App4Test {
     /**
      * Rigourous Test :-)
      */
     @Test
     public void testAll() {
-        System.out.println("TEST ALL");
+        System.out.println("TEST ALL -- JUnit 4");
         System.out.flush();
         assertTrue(true);
     }
 
     @Test
     public void testAgain() {
-        System.out.println("TEST AGAIN");
+        System.out.println("TEST AGAIN -- JUnit 4");
         System.out.flush();
         assertTrue(true);
     }
