a11c6e593b90b448155e6d14a4ffe0690896d9ac
Josef Heiss <josef.heiss@jovisco.de>
2024-09-12 21:43:24 +0200

    after section 9

--- simple-one/src/main/java/com/jovisco/mappers/UserMapper.java
+++ simple-one/src/main/java/com/jovisco/mappers/UserMapper.java
@@ -11,7 +11,8 @@ import com.jovisco.dtos.UserCreateRequestDto;
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-    
+
     UserCreateRequestDto mapToUserCreateRequestDto(User user, @MappingTarget UserCreateRequestDto dto);
+
     User mapToUser(UserCreateRequestDto dto, @MappingTarget User user);
 }
