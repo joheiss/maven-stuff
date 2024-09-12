3181da31e4b7a47ae677928671833d652dd6af1e
Josef Heiss <josef.heiss@jovisco.de>
2024-09-12 17:02:29 +0200

    1st of test-junit4

--- simple-one/src/test/java/com/jovisco/AppTest.java
+++ simple-one/src/test/java/com/jovisco/AppTest.java
@@ -1,53 +1,32 @@
 package com.jovisco;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
+public class AppTest {
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-
+    @Test
     public void testAll() {
         System.out.println("TEST ALL");
         System.out.flush();
         assertTrue(true);
     }
 
+    @Test
     public void testAgain() {
         System.out.println("TEST AGAIN");
         System.out.flush();
         assertTrue(true);
     }
 
+    @Test
     public void testHi() {
         var app = new App();
         var result = app.hi();
