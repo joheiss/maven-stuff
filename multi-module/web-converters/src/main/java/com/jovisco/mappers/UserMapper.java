44c1abee5c16c877024c464e9e340f62223c1b79
Josef Heiss <josef.heiss@jovisco.de>
2024-09-13 18:34:57 +0200

    enforcer plugin

--- multi-module/web-converters/src/main/java/com/jovisco/mappers/UserMapper.java
+++ multi-module/web-converters/src/main/java/com/jovisco/mappers/UserMapper.java
@@ -2,12 +2,15 @@ package com.jovisco.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
 
 import com.jovisco.domain.UserCommand;
 import com.jovisco.entities.User;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     User toUser(UserCommand dto);
 
