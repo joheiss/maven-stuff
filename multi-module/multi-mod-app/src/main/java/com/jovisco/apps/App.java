5785e02bb4024f15e6293ec58cf1ee964906b297
Josef Heiss <josef.heiss@jovisco.de>
2024-09-13 13:28:21 +0200

    with xsd generation

---
+++ multi-module/multi-mod-app/src/main/java/com/jovisco/apps/App.java
@@ -0,0 +1,8 @@
+package com.jovisco.apps;
+
+public class App {
+
+    public String hi() {
+        return "Hi user, what can I do for you?";
+    }
+}
---
+++ multi-module/multi-mod-app/src/test/java/com/jovisco/apps/AppTest.java
@@ -0,0 +1,9 @@
+package com.jovisco.apps;
+
+public class AppTest {
+
+    public void testAll() {
+        System.out.println("TEST ALL");
+        System.out.flush();
+    }
+}
